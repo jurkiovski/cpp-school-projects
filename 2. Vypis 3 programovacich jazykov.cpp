2caea2fc40ebc1ea1e873fbc5616e8f75f65db8b
jurkiovsky <78594369+jurkiovski@users.noreply.github.com>
2021-02-05 12:04:28 +0100

    Add files via upload
    Program shows 3 languages, in the comment is written what are those languages used for.

---
+++ 2. Vypis 3 programovacich jazykov.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	cout<<"Javascript"<<endl; //tvorba webových aplikacii
+	cout<<"PHP"<<endl; //tvorba scriptov
+	cout<<"C plus plus"<<endl; //tvorba pc aplikacii
+	return 0;
+}
