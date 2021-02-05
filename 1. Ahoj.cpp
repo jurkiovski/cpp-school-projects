d701d00833981fe6d7abd44d482999c52c93d10f
jurkiovsky <78594369+jurkiovski@users.noreply.github.com>
2021-02-05 11:54:29 +0100

    Add files via upload

---
+++ 1. Ahoj.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	cout<<"Ahoooooooooooj\t\t\t Dufam ze sa mas dobre"<<endl;
+	/* Aeeeej cas na komentárik
+	ktorý je dlhší ako jeden riadok :) */
+	cout<<"Dnes vecer streamuje stryco Donkies";
+	//chudobny komentar na jeden riadocek
+	return 0;
+}
