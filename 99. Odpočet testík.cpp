96e3a05e5fbdf9a152d47e44ed00f694cf2125d0
jurkiovsky <78594369+jurkiovski@users.noreply.github.com>
2021-02-22 00:07:22 +0100

    Update 99. Odpočet testík.cpp
    zmenil som sposob zadavania datumov a zmazal som vypocet rozdielu datumov kvoli nepresnosti,

--- 99. Odpočet testík.cpp
+++ 99. Odpočet testík.cpp
@@ -1,32 +1,43 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main(){
-	int tden, tmesiac, trok, uden, umesiac, urok, dnido;
+	int tdatum, udatum, n8, n7, n6, n5, n4, n3, n2, n1, den, mesiac, rok,
+	un8, un7, un6, un5, un4, un3, un2, un1, uden, umesiac, urok;
 	string udalost;
 	cout<<"Odpocet dni"<<endl;
-	cout<<"Zadaj den\t";
-	cin>> tden;
-	cout<<"Zadaj mesiac\t";
-	cin>> tmesiac;
-	cout<<"Zadaj rok\t";
-	cin>> trok;
-	cout<<"Dnesny datum je\t"<<tden;
-	cout<<"."<<tmesiac;
-	cout<<"."<<trok<<endl;
-	cout<<"Zadaj nazov udalosti:\t";
+	cout<<"Zadaj dnesny datum vo formate ddmmyyyy\t";
+	cin>>tdatum;
+	//rozdelenie cisla do viacerých integerov
+	n1=tdatum/10000000%10;
+	n2=tdatum/1000000%10;
+	n3=tdatum/100000%10;
+	n4=tdatum/10000%10;
+	n5=tdatum/1000%10;
+	n6=tdatum/100%10;
+	n7=tdatum/10%10;
+	n8=tdatum%10; 
+	//spojenie viacerých integerov (vedla seba)
+	rok=(n5*1000)+(n6*100)+(n7*10)+n8;
+	mesiac=(n3*10)+n4;
+	den =(n1*10)+n2;
+	cout<<"Dnesny datum je "<<den;cout<<"."<<mesiac;cout<<"."<<rok<<endl;
+	cout<<"Zadaj nazov udalosti (jednym slovom): ";
 	cin>>udalost;
-	cout<<"Den udalosti:\t";
-	cin>>uden;
-	cout<<"Mesiac udalosti:\t";
-	cin>>umesiac;
-	cout<<"Rok udalosti:\t";
-	cin>>urok;
-	cout<<"Datum udalosti - "<<udalost<<endl;
-	dnido = trok * 365 - urok * 365 + 1 * 30 - 1 * 30 + tden - uden;
-	cout<<"Do"<<udalost;
-	cout<<" zostava"<<dnido;
-	cout<<" dni."
+	cout<<"Zadaj datum udalosti vo formate ddmmyyyy\t";
+	cin>>udatum;
+	un1=udatum/10000000%10;
+	un2=udatum/1000000%10;
+	un3=udatum/100000%10;
+	un4=udatum/10000%10;
+	un5=udatum/1000%10;
+	un6=udatum/100%10;
+	un7=udatum/10%10;
+	un8=udatum%10;
+	urok=(un5*1000)+(un6*100)+(un7*10)+un8;
+	umesiac=(un3*10)+un4;
+	uden=(un1*10)+un2;
+	cout<<"Datum udalosti je "<<uden;cout<<"."<<umesiac;cout<<"."<<urok<<endl;
+	//vypocet rozdielu medzi datumami -  na vypocet bude treba viacero ifou pre urcenie dlzky jednotlivých mesiacov
 	return 0;
 }
